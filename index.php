16f57f4c24bde6637fbc4f3db8539c6d17d6aebe
tmitschke <tmitschke@MitschkeWin8.athome.local>
2013-06-25 20:53:09 +0200

    initial release

--- index.php
+++ index.php
@@ -35,9 +35,8 @@ echo '<img src="tmp/qrcode.png" alt="QR-Code missing :-(">';
 <a href='https://github.com/NTICompass/PHP-Base32'>BASE32 Implementation in PHP by NTICompass/Eric Siegel aka Rocket Hazmat<a></br>
 <a href='http://phpqrcode.sourceforge.net/'>QR-code generator in PHP by deltalab/Dominik Dzienia<a></br>
 <a href='http://php.net/manual/en/function.hash-hmac.php'>A function implementing the algorithm outlined in RFC 6238 by Pete Walker <a></br>
-
-
-<?php include 'include/counter.php'; ?>
+</br>
+</br>
 <p>See even more interesting stuff on <a href='http://www.supagusti.tk/'>www.supagusti.tk<a></br>
 Get the source code on <a href='https://github.com/supagusti/onetimepassword/'>Github<a></p></br>            
 
