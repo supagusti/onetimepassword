e95e03488b7687c7dcb98efd778aa9ccc87ec0fa
tmitschke <tmitschke@MitschkeWin8.athome.local>
2013-09-07 09:09:46 +0200

    initial release

---
+++ include/seedhex.php
@@ -0,0 +1,19 @@
+function seedhex($seed)
+{
+    $finalstring="";
+    
+    // as long as the string is, 2 steps
+    for ($i;($i<strlen($seed));$i=$i+2)
+    {   
+        //take 2 chars from the string
+        $hexpart=substr($seed, $i,2); 
+        // convert it to dec
+        $decpart=  hexdec($hexpart);
+        // get the string value of the dec value
+        $stringequi=chr($decpart);
+        // add the string together
+        $finalstring=$finalstring.$stringequi;
+    }
+    // return the string
+    return $finalstring;
+}
