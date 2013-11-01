86bcf8cc20dd97d02c298a1ab4a19963688212df
tmitschke <tmitschke@MitschkeWin8.athome.local>
2013-11-01 17:09:00 +0100

    added Google ADWords

--- totp.php
+++ totp.php
@@ -27,3 +27,14 @@
 <a href="index.php">Return to main screen</a>
 </BR>
 <iframe src="http://rcm-de.amazon.de/e/cm?t=supagusti-21&o=3&p=48&l=ur1&category=computer&banner=160GN795Y7B8SC5D9202&f=ifr" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+<br>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- SACM_BANNER -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-6013764072977718"
+     data-ad-slot="8698551187"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<br>
